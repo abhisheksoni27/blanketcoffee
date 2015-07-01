f286b0d0658bef4c02bc1cc22429bc92d9fea37e
Abhishek Soni <abbie27soni@gmail.com>
2015-07-01 14:24:15 +0530

    Fourth Commit

--- client/Posts.js
+++ client/Posts.js
@@ -21,6 +21,89 @@ Template.writeapost.events({
 
 });
 
+Template.postpage.helpers({
+exists:function(){
+
+    if(Posts.find({slug:document.URL.split('/')[3]}).count() > 0)
+        {return true;}
+    else{return false;}
+}
+});
+
+Template.dashboard.helpers({
+    posts: function() {
+        return Posts.find({});
+    }
+
+})
+
+Template.dashboard.events({
+    'click .fa-close': function() {
+        if (confirm("Are you sure")) {
+            Posts.remove(this._id)
+        }
+
+    }
+});
+
+Template.editpost.rendered = function() {
+    var agn = document.URL.split('/')[3];
+    var bodypost = Posts.find({
+        _id: agn
+    }).fetch()[0].md;
+    $("pre span").text(bodypost);
+}
+
+
+Template.editpost.events({
+    'click .publish1': function(e) {
+        e.preventDefault();
+
+        var currentPostId = this._id;
+
+        var postProperties = {
+            title: $('.entry-title input').val(),
+            body: Session.get('editor-html')
+        }
+
+        Posts.update(currentPostId, {
+            $set: postProperties
+        }, function(error) {
+            if (error) {
+                // display the error to the user
+                console.log(error.reason);
+            } else {
+                Router.go('/dashboard', {
+                    slug: Posts.find(currentPostId).slug
+                });
+            }
+        });
+    }
+});
+
+
+
+Template.dashboard.rendered = function() {
+    window.onload = function() {
+
+        function windowH() {
+            var wH = $(window).height();
+
+            $('.bluegray').css({
+                height: wH
+            });
+
+            $('.blue').css({
+                height: wH
+            });
+        }
+
+        windowH();
+
+    }
+
+}
+
 Template.writeapost.helpers({
     name: function() {
         return Meteor.user().profile.name;
@@ -75,6 +158,7 @@ Template.editor1.events({
 
 
         var bodyVar = Session.get('editor-html');
+        var bodymd = Session.get('editor-markdown');
 
 
         console.log(bodyVar);
@@ -103,7 +187,7 @@ Template.editor1.events({
                 body: bodyVar,
                 nop: numberofposts,
                 date: new Date(),
-
+                md: bodymd,
                 slug: formatSlug(titleVar)
 
 
@@ -112,8 +196,10 @@ Template.editor1.events({
 
             function currentslug() {
                 console.log("window.location");
-                var lastpost = Posts.find({id:Meteor.userId()}).count();
-                var clug = Posts.find().fetch()[lastpost-1].slug;
+                var lastpost = Posts.find({
+                    id: Meteor.userId()
+                }).count();
+                var clug = Posts.find().fetch()[lastpost - 1].slug;
                 window.location = "//localhost:3000/" + clug;
 
             }
--- client/routes.js
+++ client/routes.js
@@ -8,13 +8,28 @@ Router.route('/', function() {
     this.render('home');
 });
 
+
 Router.route('/writeapost');
+Router.route('/dashboard');
+Router.map(function() {
+
+    this.route('editpost', {
+
+        path: '/:_id/editpost',
+        data: function() {
+            return Posts.findOne(this.params._id);
+        }
+    });
+});
+
+
 
 Router.route('/:slug', {
     name: 'postpage',
     data: function() {
-        return Posts.findOne({
+        var data = Posts.findOne({
             'slug': this.params.slug
         });
+        return data;
     }
 });
